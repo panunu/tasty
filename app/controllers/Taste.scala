34374815c96df747efe523221909a50a04b7ca1c
Panu Leppäniemi <me@panuleppaniemi.com>
2013-08-08 22:44:27 +0300

    Lus

--- app/controllers/Taste.scala
+++ app/controllers/Taste.scala
@@ -3,10 +3,12 @@ package controllers
 import play.api.mvc._
 import play.api.db._
 import play.api.Play.current
+import models.Tastes
 
 object Taste extends Controller {
 
   def list = Action {
+    // TODO: Move to Service.
     val connection = DB.getConnection()
 
     Ok("Lus")
