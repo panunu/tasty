8785297e2c304a6cac41281323c7a3eae51f8c4b
Panu Leppäniemi <me@panuleppaniemi.com>
2013-08-08 21:47:33 +0300

    Model

--- app/models/Taste.scala
+++ app/models/Taste.scala
@@ -3,6 +3,10 @@ package models
 import play.api.db.slick.Config.driver.simple._
 
 object Tastes extends Table[(Long, String, String, Int)]("tastes") {
-  def id   = column[Long]("id", O.PrimaryKey)
-  def name = column[String]("name", O.NotNullable)
+  def id = column[Long]("id", O.PrimaryKey)
+  def name = column[String]("name")
+  def description = column[String]("description")
+  def rating = column[Int]("rating")
+
+  def * = id ~ name ~ description ~ rating
 }
