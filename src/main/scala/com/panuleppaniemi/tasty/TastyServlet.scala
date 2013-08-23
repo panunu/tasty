f8811de7ba5f4d64540e6b82ec08922b37c4b678
Panu Leppäniemi <me@panuleppaniemi.com>
2013-08-23 23:41:43 +0300

    No templating

--- src/main/scala/com/panuleppaniemi/tasty/TastyServlet.scala
+++ src/main/scala/com/panuleppaniemi/tasty/TastyServlet.scala
@@ -3,15 +3,10 @@ package com.panuleppaniemi.tasty
 import org.scalatra._
 import scalate.ScalateSupport
 
-class TastyServlet extends TastyStack {
+class TastyServlet extends ScalatraServlet {
 
   get("/") {
-    <html>
-      <body>
-        <h1>Hello, world!</h1>
-        Say <a href="hello-scalate">hello to Scalate</a>.
-      </body>
-    </html>
+    "Hello"
   }
   
 }
