6af776db05c12fe7ae012503ccbf2906ded56301
Panu Leppäniemi <me@panuleppaniemi.com>
2013-08-25 12:32:07 +0300

    TODO

--- src/test/scala/com/panuleppaniemi/tasty/controllers/TasteControllerSpec.scala
+++ src/test/scala/com/panuleppaniemi/tasty/controllers/TasteControllerSpec.scala
@@ -3,6 +3,7 @@ package com.panuleppaniemi.tasty.controllers
 import org.scalatra.test.specs2._
 
 // For more on Specs2, see http://etorreborre.github.com/specs2/guide/org.specs2.guide.QuickStart.html
+// TODO: Fixtures and a test database.
 class TasteControllerSpec extends ScalatraSpec { def is =
   "GET / on TasteController" ^
     "should return status 200" ! status200^
