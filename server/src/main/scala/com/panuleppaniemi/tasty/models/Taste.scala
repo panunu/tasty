7322fa862a886494073066932b463068ff72a9fc
Panu Leppäniemi <me@panuleppaniemi.com>
2013-10-03 21:57:35 +0300

    Lus

--- server/src/main/scala/com/panuleppaniemi/tasty/models/Taste.scala
+++ server/src/main/scala/com/panuleppaniemi/tasty/models/Taste.scala
@@ -2,14 +2,13 @@ package com.panuleppaniemi.tasty.models
 
 import scala.slick.driver.MySQLDriver.simple._
 
-case class Taste(id: Option[Long] = None, name: String, description: String, rating: Int, tags: Array[Byte])
+case class Taste(id: Option[Long] = None, name: String, description: String, rating: Int)
 
 object Tastes extends Table[Taste]("tastes") {
   def id = column[Long]("id", O.PrimaryKey, O.AutoInc)
   def name = column[String]("name")
   def description = column[String]("description")
   def rating = column[Int]("rating")
-  def tags = column[Array[Byte]]("tags")
 
-  def * = id.? ~ name ~ description ~ rating ~ tags <> (Taste, Taste.unapply(_))
+  def * = id.? ~ name ~ description ~ rating <> (Taste, Taste.unapply(_))
 }
