fe303753f312d98f8a3b70abd044be778f91c695
Panu Leppäniemi <me@panuleppaniemi.com>
2013-10-04 23:23:48 +0300

    TODO

--- server/src/main/scala/com/panuleppaniemi/tasty/services/TagService.scala
+++ server/src/main/scala/com/panuleppaniemi/tasty/services/TagService.scala
@@ -29,7 +29,7 @@ class TagService(database: Database) {
 
   def tag(taste: Taste, tags: List[Tag]): List[Tagging] = tags.map(tag(taste, _))
 
-  def tag(taste: Taste, tag: Tag) = database.connection.withSession {
+  def tag(taste: Taste, tag: Tag) = database.connection.withSession { // TODO: Map to a Case class?
     val tagging = new Tagging(None, taste.id.getOrElse(0), tag.id.getOrElse(0))
     tagging.copy(id=Some(Taggings returning Taggings.id insert(tagging)))
   }
