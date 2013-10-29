698d71793dc53b22cf238b3e874e2cbe18150df2
Panu Leppäniemi <me@panuleppaniemi.com>
2013-10-29 18:47:59 +0200

    Eradications

--- server/project/build.scala
+++ server/project/build.scala
@@ -13,7 +13,7 @@ object TastyBuild extends Build {
   lazy val project = Project (
     "tasty",
     file("."),
-    settings = seq(com.typesafe.startscript.StartScriptPlugin.startScriptForClassesSettings: _*) ++ Defaults.defaultSettings ++ ScalatraPlugin.scalatraWithJRebel ++ Seq(
+    settings = Defaults.defaultSettings ++ ScalatraPlugin.scalatraWithJRebel ++ Seq(
       organization := Organization,
       name := Name,
       version := Version,
@@ -23,15 +23,15 @@ object TastyBuild extends Build {
         "org.scalatra" %% "scalatra" % ScalatraVersion,
         "org.scalatra" %% "scalatra-specs2" % ScalatraVersion % "test",
         "com.typesafe.slick" %% "slick" % "1.0.1",
-        "mysql" % "mysql-connector-java" % "5.1.13",
         "c3p0" % "c3p0" % "0.9.1.2",
+        "postgresql" % "postgresql" % "9.1-901-1.jdbc4",
         "ch.qos.logback" % "logback-classic" % "1.0.6" % "runtime",
         "com.softwaremill.macwire" %% "core" % "0.4",
         "com.softwaremill.macwire" %% "scopes" % "0.4",
         "org.scalatra" %% "scalatra-json" % "2.2.1",
         "org.json4s" %% "json4s-jackson" % "3.2.4",
-        "org.eclipse.jetty" % "jetty-webapp" % "8.1.8.v20121106" % "compile;container",
-        "org.eclipse.jetty.orbit" % "javax.servlet" % "3.0.0.v201112011016" % "compile;container;provided;test" artifacts (Artifact("javax.servlet", "jar", "jar"))
+        "org.eclipse.jetty" % "jetty-webapp" % "8.1.8.v20121106" % "container",
+        "org.eclipse.jetty.orbit" % "javax.servlet" % "3.0.0.v201112011016" % "container;provided;test" artifacts (Artifact("javax.servlet", "jar", "jar"))
       )
     )
   ).dependsOn(deliciousValidation)
